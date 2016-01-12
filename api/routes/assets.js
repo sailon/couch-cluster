bcf39a1e78a1eadfcd12bfdfe7f7658f14994edd
Mike Ross <rossmi@avalonconsult.com>
2016-01-12 13:27:19 -0500

    Converted indents to spaces to help with formatting

--- api/routes/assets.js
+++ api/routes/assets.js
@@ -15,7 +15,7 @@ var N1qlQuery = couchbase.N1qlQuery;
  * @returns {string}
  */
 function sanitize (input) {
-	return input.replace(/ /g, '');
+  return input.replace(/ /g, '');
 }
 
 /**
@@ -24,8 +24,8 @@ function sanitize (input) {
  * @returns {boolean}
  */
 function isValidUri (uri) {
-	var uri = sanitize(uri);
-	return (uri !== null && uri !== 'undefined' && uri.match(/\w{3,}\:\/\/\w{2,}\/\w*/g));
+  var uri = sanitize(uri);
+  return (uri !== null && uri !== 'undefined' && uri.match(/\w{3,}\:\/\/\w{2,}\/\w*/g));
 }
 
 /**
@@ -34,38 +34,38 @@ function isValidUri (uri) {
  * @returns {object}
  */
 function docQuery (uri) {
-	var str = 'SELECT uri, name, note FROM `assets` WHERE assets.uri="' + uri + '"';
-	return N1qlQuery.fromString(str);
+  var str = 'SELECT uri, name, note FROM `assets` WHERE assets.uri="' + uri + '"';
+  return N1qlQuery.fromString(str);
 }
 
 /*
  * List assets
- * GET /api/assets?size={size}&page={page}&tag={tag1,tag2}
+ * GET /api/assets?size={size}&page={page}
  */
 router.get('/', function (req, res) {
 
-	// Setting up pagination options with defaults
-	var options = {
-		perPage: parseInt(req.query.size) || 10,
-		page: parseInt(req.query.page) || 1
-	};
+  // Setting up pagination options with defaults
+  var options = {
+    perPage: parseInt(req.query.size) || 10,
+    page: parseInt(req.query.page) || 1
+  };
 
-	// Building the query string
-	var str = 'SELECT uri, name, note FROM `assets` ';
-	var pagination = (' LIMIT ' + options.perPage + ' OFFSET ' + (options.page * options.perPage - options.perPage));
-	str += pagination;
+  // Building the query string
+  var str = 'SELECT uri, name, note FROM `assets` ';
+  var pagination = (' LIMIT ' + options.perPage + ' OFFSET ' + (options.page * options.perPage - options.perPage));
+  str += pagination;
 
-	// Construct the query object
-	var query = N1qlQuery.fromString(str);
+  // Construct the query object
+  var query = N1qlQuery.fromString(str);
 
-	// Execute query against Couchbase
-	bucket.query(query,[],function(err,assets){
-		if (!err) {
+  // Execute query against Couchbase
+  bucket.query(query,[],function(err,assets){
+    if (!err) {
       return res.send(assets);
     } else {
       return console.log(err);
     }
-	});
+  });
 
 });
 
@@ -75,18 +75,18 @@ router.get('/', function (req, res) {
  */
 router.post('/', function (req, res) {
 
-	var uri = req.body.uri;
+  var uri = req.body.uri;
 
-	if (!isValidUri(uri)) return res.send('400, "uri" is a required field and must be well formed.');
+  if (!isValidUri(uri)) return res.send('400, "uri" is a required field and must be well formed.');
 
-	// Create or update an asset document
-	bucket.upsert(req.body.uri,req.body,function(err,assets){
-		if (!err) {
+  // Create or update an asset document
+  bucket.upsert(req.body.uri,req.body,function(err,assets){
+    if (!err) {
       return res.sendStatus(201);
     } else {
       return console.log(err);
     }
-	});
+  });
 });
 
 /*
@@ -95,32 +95,32 @@ router.post('/', function (req, res) {
  */
 router.delete('/', function (req, res) {
 
-	var uri = req.body.uri;
+  var uri = req.body.uri;
 
-	if (!isValidUri(uri)) return res.send('400, "uri" is a required field and must be well formed.');
+  if (!isValidUri(uri)) return res.send('400, "uri" is a required field and must be well formed.');
 
-	// Build the query to find the document
-	var query = docQuery(uri);
+  // Build the query to find the document
+  var query = docQuery(uri);
 
-	// Find the document to ensure it exists
-	bucket.query(query, [], function (err, assets){
-		var asset = assets[0]; // Query returns array, grabbing the first result
+  // Find the document to ensure it exists
+  bucket.query(query, [], function (err, assets){
+    var asset = assets[0]; // Query returns array, grabbing the first result
 
-		if (!err && asset && asset !== 'undefined') {
+    if (!err && asset && asset !== 'undefined') {
 
       // Delete the asset
-			bucket.remove(req.body.uri,function(err,assets){
-				if (!err) {
-		      return res.sendStatus(204);
-		    } else {
-		      return console.log(err);
-		    }
-			});
+      bucket.remove(req.body.uri,function(err,assets){
+        if (!err) {
+          return res.sendStatus(204);
+        } else {
+          return console.log(err);
+        }
+      });
 
     } else { // Asset does not exist
       return res.sendStatus(404);
     }
-	});
+  });
 
 });
 
@@ -130,37 +130,37 @@ router.delete('/', function (req, res) {
  */
 router.put('/', function (req, res) {
 
-	var uri = sanitize(req.body.uri);
-	var note = req.body.note;
+  var uri = sanitize(req.body.uri);
+  var note = req.body.note;
 
-	if (!isValidUri(uri)) return res.send('400, "uri" is a required field and must be well-formed.');
-	if (!note) return res.send('400, "note" is a required field.');
+  if (!isValidUri(uri)) return res.send('400, "uri" is a required field and must be well-formed.');
+  if (!note) return res.send('400, "note" is a required field.');
 
-	// Build the query to find the document
-	var query = docQuery(uri);
+  // Build the query to find the document
+  var query = docQuery(uri);
 
-	// Find the document to update
-	bucket.query(query, [], function (err, assets){
-		var asset = assets[0]; // Query returns array, grabbing the first result
+  // Find the document to update
+  bucket.query(query, [], function (err, assets){
+    var asset = assets[0]; // Query returns array, grabbing the first result
 
-		if (!err && asset && asset !== 'undefined') {
+    if (!err && asset && asset !== 'undefined') {
 
-			// Update the note field with the new submission
+      // Update the note field with the new submission
       asset.note = note;
 
       // Replace the old document with the updated document
       bucket.replace(uri, asset, function (err) {
-      	if (!err) {
-		      return res.sendStatus(200);
-		    } else {
-		      return console.log(err);
-		    }
+        if (!err) {
+          return res.sendStatus(200);
+        } else {
+          return console.log(err);
+        }
       });
 
     } else { // Asset does not exist
       return res.sendStatus(404);
     }
-	});
+  });
 });
 
 module.exports = router;
